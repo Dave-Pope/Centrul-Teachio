8246188fcd79f5863bd5fd83d393503392223d53
David <david.popescu10@gmail.com>
2019-12-31 13:11:17 +0200

    Homepage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './containers/Navbar/Navbar';
+import Homepage from './containers/Homepage/Homepage';
 
 import './style/App.css';
 
@@ -11,6 +12,7 @@ class App extends Component {
       <Router>
         <div className='App'>
           <Navbar />
+          <Homepage />
         </div>
       </Router>
     );
---
+++ src/containers/Homepage/Homepage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div className='Homepage'>
+      <img
+        className='picture'
+        src={require('../../assets/undraw_book_lover_mkck.png')}
+        alt=''
+      />
+
+      <p>
+        Un mediu bun si profesionist pentru dezoltarea ta intelectuala si profesionala
+      </p>
+    </div>
+  );
+};
+
+export default Homepage;
