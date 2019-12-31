f52d785b506bb906afad7014d4943ed71109ba9b
David <david.popescu10@gmail.com>
2019-12-31 12:52:31 +0200

    Navbar

---
+++ src/containers/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+      <h1>TEACHIO</h1>
+
+      <ul>
+        <li>
+          <a href='#'>Acasa</a>
+        </li>
+        <li>
+          <a href='#'>Despre noi</a>
+        </li>
+        <li>
+          <a href='#'>Meditatii</a>
+        </li>
+        <li>
+          <a href='#'>Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
