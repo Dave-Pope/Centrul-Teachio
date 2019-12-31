45817df4166cb0c4976befd23b2e401eba814457
David <david.popescu10@gmail.com>
2019-12-31 13:17:45 +0200

    Updates

---
+++ precache-manifest.8032ccdeff56e709244efb4fadbba9b0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b043560519814731e3ff10429238ba8",
+    "url": "/Centrul-Teachio/index.html"
+  },
+  {
+    "revision": "9bd068d0b1aecc6ea542",
+    "url": "/Centrul-Teachio/static/css/main.8aa14b78.chunk.css"
+  },
+  {
+    "revision": "65a6e309c4fee7d952b9",
+    "url": "/Centrul-Teachio/static/js/2.174b4bb7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Centrul-Teachio/static/js/2.174b4bb7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bd068d0b1aecc6ea542",
+    "url": "/Centrul-Teachio/static/js/main.65cb9cbc.chunk.js"
+  },
+  {
+    "revision": "e03183e4a0bc239b455e",
+    "url": "/Centrul-Teachio/static/js/runtime-main.bb7599e8.js"
+  },
+  {
+    "revision": "1cd294a771f26752bbb8d8d5210f6412",
+    "url": "/Centrul-Teachio/static/media/Nunito-Bold.1cd294a7.ttf"
+  },
+  {
+    "revision": "4f3d2e011a08fae118e258bcaf0dfa13",
+    "url": "/Centrul-Teachio/static/media/Nunito-ExtraLight.4f3d2e01.ttf"
+  },
+  {
+    "revision": "6a4fadd273f85b2ae8c6ffadbcf7beec",
+    "url": "/Centrul-Teachio/static/media/Nunito-Light.6a4fadd2.ttf"
+  },
+  {
+    "revision": "d2e691bc4a2b696929172cb3d22ce8ba",
+    "url": "/Centrul-Teachio/static/media/Nunito-Regular.d2e691bc.ttf"
+  },
+  {
+    "revision": "47affff67cd229907e8cf64c7ab7d65d",
+    "url": "/Centrul-Teachio/static/media/undraw_book_lover_mkck.47affff6.png"
+  }
+]);
